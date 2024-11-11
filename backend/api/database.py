71ae3e1d6d6bc170532ab2c24352c692d672c4f8
Ramish Fakhar <ramish.fakhar877@gmail.com>
2024-11-11 22:31:47 +0500

    files added

---
+++ backend/api/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+URL_DATABASE='postgresql://postgres:123456!@localhost:5432/workout'
+
+engine = create_engine(URL_DATABASE)
+Sessionlocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base = declarative_base()
